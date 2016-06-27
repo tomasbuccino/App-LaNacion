022c7c8a480f14bc056c1ea04d664fde49e70746
SirUnknown <b_tomas95@hotmail.com>
2016-06-27 11:20:41 -0300

    updates

--- app.js
+++ app.js
@@ -1,43 +1,56 @@
-$("#saludo").click(saludar); //el $ selecciona desde el css según el nombre, por eso el #. $ + # selecciona según Id. Luego se llama al evento click (en vez de usar el onclick y los parametros) y se le pasa el nombre de la funcion
+function initialize() {
 
-function saludar(){
+var gps = navigator.geolocation.getCurrentPosition(
 
-	alert("Hola");
+function (position) {
 
-}
+	 var latitude = position.coords.latitude;
+     var longitude = position.coords.longitude;
+     var geolocpoint = new google.maps.LatLng(latitude, longitude);
 
-function hay(){
+	var mapOptions = {
+  	center: geolocpoint, //{ lat: position.coords.latitude, lng: position.coords.longitude},
+  	zoom: 18,
 
-	alert("Hay Internet");
+};
 
-}
+var map = new google.maps.Map(document.getElementById('map'),
+    mapOptions);
+ 
+var you = new google.maps.Marker({
+    position: geolocpoint,
+    map: map,
+    title: "Dis be You!",
+    animation: google.maps.Animation.DROP 
 
-function noHay(){
+	});
 
-	alert("No hay Internet");
+var villagecaballito = new google.maps.Marker({
+    position: new google.maps.LatLng(-34.6185305, -58.4374863),
+    map: map,
+    title: "Village Caballito!",
+    animation: google.maps.Animation.DROP 
 
-}
+	});
 
-if(navigator.offline){
-	alert("No hay señal")
-}else{
-	alert("Hay señal")
-}
+var atlasflores = new google.maps.Marker({
+    position: new google.maps.LatLng(-34.629335, -58.462382),
+    map: map,
+    title: "Atlas Flores!",
+    animation: google.maps.Animation.DROP 
 
-function guardar(){
-	localStorage.setItem("nombre", "Pepe"); //le paso la variable y el valor asociado
+	});
 
-}
+var hoytsabasto = new google.maps.Marker({
+    position: new google.maps.LatLng(-34.603329, -58.410789),
+    map: map,
+    title: "Hoyts Abasto!",
+    animation: google.maps.Animation.DROP 
 
-function ver(){
-	alert (localStorage.getItem("nombre")); //solo getteo la variable
-
-}
+	});
 
 
-$("#btn_guardar").click(guardar);
 
-$("#btn_ver").click(ver);
-
-//guardar datos localmente en el celular
-
+ }
+ );
+};
